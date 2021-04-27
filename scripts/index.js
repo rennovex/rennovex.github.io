22747d9f6823e11ef17b84d333e8a3ecb02f6511
Neeraj-SD <neerajsd04@gmail.com>
2021-04-27 22:42:47 +0530

    navbar mobile version 1st version

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+// Nav animation script !!!!!!
+
+
+/*const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = 'flex';
+}*/
